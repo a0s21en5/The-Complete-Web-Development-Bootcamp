568cc77058cbf42737b7ddc2409ef578bfce47d5
Ashish Sahu <86140629+a0s21en5@users.noreply.github.com>
2022-12-28 16:51:27 +0000

    Update

---
+++ 9 - Introduction to Javascript ES6/function_age.js
@@ -0,0 +1,14 @@
+let a = prompt("Enter u'r age: ")
+
+Voter(a)
+
+function Voter( a ){
+
+    if (a>= 18){
+        alert('You are able to Vote')
+    }
+    else{
+        alert("U can not vote!")
+    }
+}
+
---
+++ 9 - Introduction to Javascript ES6/operator.js
@@ -0,0 +1,10 @@
+let a  = 8, b = "8";
+
+
+// triple equal checks both the values & the data types
+if (a === b){
+    console.log("inside If statement")
+}
+else {
+    console.log("inside else statement")
+}
